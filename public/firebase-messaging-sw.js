918a6e0ad0ddc6bc2df0125da0962e5fd2f937b8
abidali05 <mhrabid558@gmail.com>
2024-03-12 11:55:00 +0500

    pushNotificationFirebaseLaravel

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+/*
+Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+*/
+firebase.initializeApp({
+    apiKey: "AIzaSyCDhazyI0Z21CGJNeLyKjekHkvnSEWR1rg",
+        authDomain: "push-notification-6495d.firebaseapp.com",
+        projectId: "push-notification-6495d",
+        storageBucket: "push-notification-6495d.appspot.com",
+        messagingSenderId: "410708048724",
+        appId: "1:410708048724:web:b2dd471bab46edfe2d26c1"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log("Message received.", payload);
+    const title = "Hello world is awesome";
+    const options = {
+        body: "Your notificaiton message .",
+        icon: "/firebase-logo.png",
+    };
+    return self.registration.showNotification(
+        title,
+        options,
+    );
+});
